e69290b734cb8e72efc60ac099d23be859f79cef
dontstopmeowing <dnny419@gmail.com>
2021-12-10 08:27:32 +0700

    Adding task 0.

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+def canUnlockAll(boxes):
+    """
+        Function that determines if all the boxes can be opened.
+    """
+    keys = [0]
+    for key in keys:
+        for i in boxes[key]:
+            if i not in keys and i < len(boxes):
+                keys.append(i)
+    if len(keys) == len(boxes):
+        return True
+    return False
