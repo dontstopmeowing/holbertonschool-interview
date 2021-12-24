350e679f2f8cba86eaf909a8b33ce51cc1130926
dontstopmeowing <dnny419@gmail.com>
2021-12-24 07:56:51 +0700

    Adding initial files.

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node.
+ * @parent: ptr to the parent node
+ * @value: value to be added in the new node.
+ * Return: ptr address to new node otherwise NULL.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+
+	return (new);
+}
